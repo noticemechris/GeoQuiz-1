c196760d14b0209c619f3f3d8b6bc21921f2fd96
Adam Goodman <59903108+noticemeaikas@users.noreply.github.com>
2020-02-23 20:35:44 -0800

    Fixed David's spelling of "river"

--- app/src/main/java/org/overlake/mat803/geoquiz/QuizActivity.java
+++ app/src/main/java/org/overlake/mat803/geoquiz/QuizActivity.java
@@ -36,6 +36,7 @@ public class QuizActivity extends AppCompatActivity {
                 new Question(R.string.question_mideast, false),
                 new Question(R.string.question_africa, false),
                 new Question(R.string.question_americas, true),
+                new Question(R.string.question_chris, true),
                 new Question(R.string.question_asia, true)
         };
     }
